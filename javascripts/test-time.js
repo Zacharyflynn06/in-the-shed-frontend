7a9b8d695ca77c438c8beb43bd8419cd0b84f253
Zac <zacharyflynn06@gmail.com>
2021-06-05 17:49:49 -0600

    begin adding timer class

--- javascripts/test-time.js
+++ javascripts/test-time.js
@@ -1,31 +1,31 @@
-let timeout
+// let timeout
 
 
 
-const startTest = () => {
-    console.log('Started')
-    console.log(this)
-    const startTime = Date.now()
-    let totalTime = 0
-    const round = () => {
-        timeout = setTimeout(() => {
-            totalTime += 1000
-            let elapsedTime = Date.now() - startTime
-            console.log('Total Drift', elapsedTime - totalTime)
-            round()
-            }, 1000)
-    }
+// const startTest = () => {
+//     console.log('Started')
+//     console.log(this)
+//     const startTime = Date.now()
+//     let totalTime = 0
+//     const round = () => {
+//         timeout = setTimeout(() => {
+//             totalTime += 1000
+//             let elapsedTime = Date.now() - startTime
+//             console.log('Total Drift', elapsedTime - totalTime)
+//             round()
+//             }, 1000)
+//     }
 
-    round()
-}
+//     round()
+// }
 
-const stopTest = () => {
-    clearTimeout(timeout)
-    console.log('Stopped')
-}
+// const stopTest = () => {
+//     clearTimeout(timeout)
+//     console.log('Stopped')
+// }
 
-document.addEventListener("DOMContentLoaded", function(){
-    startTest()
-})
+// document.addEventListener("DOMContentLoaded", function(){
+//     startTest()
+// })
 
-// startStopBtn().addEventListener("click", startTest)
+// // startStopBtn().addEventListener("click", startTest)
---
+++ javascripts/timer.js
@@ -0,0 +1,22 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     startStopBtn().addEventListener('click', function(){hi.play()})
+// })
+
+class Timer{
+    constructor(callback, interval) {
+        this.interval = interval
+    }
+
+    start() {
+        
+    }
+
+    stop() {
+
+    }
+
+    round() {
+
+    }
+
+}
