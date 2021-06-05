aed1902f17d293c7b097895cdabd746bb677f9ce
Zac <zacharyflynn06@gmail.com>
2021-06-05 16:15:36 -0600

    add metMArkings

--- javascripts/metronome.js
+++ javascripts/metronome.js
@@ -5,11 +5,30 @@ const increaseMetBtn = () => document.querySelector('.increase-tempo')
 const tempoRange = () => document.querySelector('.tempo-slider')
 const startStopBtn = () => document.querySelector('.start-stop-button')
 
-let bpm = 200
+let bpm = 100
+let metMarking = 'Moderato'
+
+const setMetMarking = () => {
+    if (bpm <= 40 ) {metMarking = "Grave"}
+    if (bpm > 40 && bpm <= 60 ) {metMarking = "Largo"}
+    if (bpm > 60 && bpm <= 66 ) {metMarking = "Larghetto"}
+    if (bpm > 66 && bpm <= 72 ) {metMarking = "Adagio"}
+    if (bpm > 72 && bpm <= 83 ) {metMarking = "Andante"}
+    if (bpm > 83 && bpm <= 100 ) {metMarking = "Moderato"}
+    if (bpm > 100 && bpm <= 120 ) {metMarking = "Allegretto"}
+    if (bpm > 120 && bpm <= 156 ) {metMarking = "Allegro"}
+    if (bpm > 156 && bpm <= 176 ) {metMarking = "Vivace"}
+    if (bpm > 176 && bpm <= 200 ) {metMarking = "Presto"}
+    if (bpm > 200) {metMarking = "Prestissimo"}
+
+    tempoText().innerText = metMarking
+}
 
 tempoRange().addEventListener('input', () => {
     bpm = tempoRange().value
     tempoDisplay().textContent = bpm
+    tempoRange().value = bpm
+    setMetMarking()
 })
 
 decreaseMetBtn().addEventListener('click', () => {
@@ -17,11 +36,16 @@ decreaseMetBtn().addEventListener('click', () => {
     bpm--
     tempoDisplay().textContent = bpm
     tempoRange().value = bpm
-} )
+    setMetMarking()
+})
 
 increaseMetBtn().addEventListener('click', () => {
-    if (bpm >= 300) {return}
+    if (bpm >= 250) {return}
     bpm++
     tempoDisplay().textContent = bpm
     tempoRange().value = bpm
-} )
+    setMetMarking()
+})
+
+
+
