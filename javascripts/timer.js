1ee976af89ab57c3cb3b1f730accd40a4f75407a
Zac <zacharyflynn06@gmail.com>
2021-06-05 19:13:21 -0600

    revise Timer class for less drift by putting all functions in the constructor

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -2,28 +2,53 @@
 //     startStopBtn().addEventListener('click', function(){hi.play()})
 // })
 
+
+// function Timer(callback, timeInterval) {
+//     this.timeInterval = timeInterval
+
+//     this.start = () => {
+//         this.expected = Date.now() + this.timeInterval;
+//         this.timeout = setTimeout(this.round, this.timeInterval);
+//         console.log('started')
+//     }
+
+//     this.stop = () => {
+//         clearTimeout(this.timeout)
+//         console.log("stoped")
+//     }
+
+//     this.round = () => {
+//         let drift = Date.now() - this.expected
+//         callback()
+//         this.expected += this.timeInterval
+//         console.log(drift)
+//     }
+// }
+
 class Timer{
     constructor(callback, interval) {
+
         this.interval = interval
         this.callback = callback
-    }
 
-    startTime() {
-        console.log(this)
-        this.expectedTime = Date.now() + this.interval
-        this.timeout = setTimeout(this.round, this.interval)
-        console.log("started")
-    }
+        this.start = () => {
+            this.expectedTime = Date.now() + this.interval
+            this.timeout = setTimeout(this.cycle, this.interval)
+            console.log("started")
+        }
 
-    stopTime() {
-        clearTimeout(this.timeout)
-        console.log("stop")
-    }
+        this.cycle = () => {
+            const drift = Date.now() - this.expectedTime
+            this.expectedTime += this.interval
+            console.log("drift", drift)
+        }
 
-    round() {
-        this.callback
+        this.stop = () => {
+            clearTimeout(this.timeout)
+            console.log("stop")
+        }
     }
-
 }
 
-const let timer = new Timer(() => {console.log("it works")}, 1000)
+const myTimer = new Timer(() => {console.log("it works")}, 1000)
+myTimer.start()
