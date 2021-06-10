4045e2dfa73c41408fe63fba759d7a79f66c3427
Zac <zacharyflynn06@gmail.com>
2021-06-10 15:15:34 -0600

    add constructor for song model

--- javascripts/models/song.js
+++ javascripts/models/song.js
@@ -2,6 +2,19 @@ class Song {
 
     static all = []
 
-    constructor({user, })
+    constructor({user, id, title, tempo, author, measures = []}){
+        this.user = user
+        this.id = id
+        this.title = title
+        this.tempo = tempo
+        this.author = author
+        this.measures = measures
+
+        Song.all.push(this)
+    }
+
+    static getAll() {
+        return this.all
+    }
     
 }
